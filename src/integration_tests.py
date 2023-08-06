65c69e28a4f6b0198d0abcbbf20cdb68258fedad
Paul Boschert <paul@boschert.net>
2023-08-06 16:54:28 -0600

    adding integration test file

---
+++ src/integration_tests.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from subprocess import Popen
+from Client import Client
+
+def test_server_client():
+    '''
+    We start the server and let it run in the background. Then we ask 
+    the client to make a call to the server and we compare the expected value.
+    '''
+    server = Popen('./src/Server.py')
+    client = Client()
+    sum = client.get_sum(3, 4)
+    assert (sum == 7)
